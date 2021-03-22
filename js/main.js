153b77a58381cca130e82b7d6ccd73c8945344c2
Cyril Bochet <bochet66@gmail.com>
2021-03-22 18:58:52 +0100

    premiere version

--- js/main.js
+++ js/main.js
@@ -7,60 +7,72 @@ $(document).ready(function () {
             "titre": "Multi sites Magento",
             "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
                 "Paramétrage de modules et amélioration de l'expérience utilisateur",
-            "thumbnail": "assets/img/portfolio/cabin.png",
-            "image": "assets/img/portfolio/cabin.png",
+            "image": "assets/img/portfolio/image1.jpg",
             "tag": ["MAGENTO", "MULTISITE"],
-            "filter": "web",
         },
         {
             "id": 2,
             "titre": "Multi sites Magento",
             "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
                 "Paramétrage de modules et amélioration de l'expérience utilisateur",
-            "thumbnail": "assets/img/portfolio/cabin.png",
-            "image": "assets/img/portfolio/cabin.png",
+            "image": "assets/img/portfolio/image2.jpg",
             "tag": ["MAGENTO", "MULTISITE"],
-            "filter": "web",
         },
         {
             "id": 3,
             "titre": "Multi sites Magento",
             "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
                 "Paramétrage de modules et amélioration de l'expérience utilisateur",
-            "thumbnail": "assets/img/portfolio/cabin.png",
-            "image": "assets/img/portfolio/cabin.png",
+            "image": "assets/img/portfolio/image6.jpg",
             "tag": ["MAGENTO", "MULTISITE"],
-            "filter": "web",
         },
         {
             "id": 4,
             "titre": "Multi sites Magento",
             "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
                 "Paramétrage de modules et amélioration de l'expérience utilisateur",
-            "thumbnail": "assets/img/portfolio/cabin.png",
-            "image": "assets/img/portfolio/cabin.png",
+            "image": "assets/img/portfolio/image4.jpg",
             "tag": ["MAGENTO", "MULTISITE"],
-            "filter": "web",
         },
         {
             "id": 5,
             "titre": "Multi sites Magento",
             "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
                 "Paramétrage de modules et amélioration de l'expérience utilisateur",
-            "thumbnail": "assets/img/portfolio/cabin.png",
-            "image": "assets/img/portfolio/cabin.png",
+            "image": "assets/img/portfolio/image5.jpg",
             "tag": ["MAGENTO", "MULTISITE"],
-            "filter": "web",
         },
         {
             "id": 6,
             "titre": "Multi sites Magento",
             "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
                 "Paramétrage de modules et amélioration de l'expérience utilisateur",
-            "thumbnail": "assets/img/portfolio/cabin.png",
-            "image": "assets/img/portfolio/cabin.png",
+            "image": "assets/img/portfolio/image3.jpg",
             "tag": ["MAGENTO", "MULTISITE"],
-            "filter": "web",
+        },
+        {
+            "id": 7,
+            "titre": "Multi sites Magento",
+            "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
+                "Paramétrage de modules et amélioration de l'expérience utilisateur",
+            "image": "assets/img/portfolio/image7.jpg",
+            "tag": ["MAGENTO", "MULTISITE"],
+        },
+        {
+            "id": 8,
+            "titre": "Multi sites Magento",
+            "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
+                "Paramétrage de modules et amélioration de l'expérience utilisateur",
+            "image": "assets/img/portfolio/image8.jpg",
+            "tag": ["MAGENTO", "MULTISITE"],
+        },
+        {
+            "id": 9,
+            "titre": "Multi sites Magento",
+            "description": "Amélioration et mise à jour de 3 sites e-commerce sur Magento. \n" +
+                "Paramétrage de modules et amélioration de l'expérience utilisateur",
+            "image": "assets/img/portfolio/image9.jpg",
+            "tag": ["MAGENTO", "MULTISITE"],
         },
     ]
     createProjects(projects);
@@ -76,7 +88,7 @@ function createProjects(projects) {
             '                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">\n' +
             '                        <div class="portfolio-item-caption-content text-center text-white"><i class="fas fa-search-plus fa-3x"></i></div>\n' +
             '                    </div>\n' +
-            '                    <img class="img-fluid" src="' + projects[i]["thumbnail"] + '" alt=""/>\n' +
+            '                    <img class="img-fluid" src="' + projects[i]["image"] + '" alt=""/>\n' +
             '                </div>\n' +
             '            </div>';
 
@@ -93,7 +105,7 @@ function createProjects(projects) {
             '                    <div class="row justify-content-center">\n' +
             '                        <div class="col-lg-12">\n' +
             '                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-4"\n' +
-            '                                id="portfolioModal2Label">' + projects[i]["title"] + '</h2>\n' +
+            '                                id="portfolioModal2Label">' + projects[i]["titre"] + '</h2>\n' +
             '                            <p class="mb-3">' + projects[i]["description"] + '</p>\n' +
             '                            <img class="img-fluid mb-3" src="' + projects[i]["image"] + '" alt=""/>\n' +
             '                        </div>\n' +
